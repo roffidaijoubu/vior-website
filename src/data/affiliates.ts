6b73eef6cdd94f7d345d7589c869b4d01ddfd053
Developer <prama13@gmail.com>
2024-10-07 03:05:06 +0700

    Working Video Lists

--- src/data/affiliates.ts
+++ src/data/affiliates.ts
@@ -6,6 +6,7 @@ export interface Affiliate {
   description: string;
   image: string;
   socialLinks: { platform: string; url: string }[];
+  pastWorks?: { title: string; youtubeUrl: string }[]; // Add this line
 }
 
 export const affiliates: Affiliate[] = [
@@ -18,6 +19,22 @@ export const affiliates: Affiliate[] = [
       { platform: "Twitter", url: "https://twitter.com/ahomika_ch" },
       { platform: "YouTube", url: "https://www.youtube.com/@ahomika_ch" },
       { platform: "Tiktok", url: "https://www.tiktok.com/@ahomika_ch" },
+    ],
+    pastWorks: [
+      { title: "Deception 【ORIGINAL SONG】", youtubeUrl: "https://www.youtube.com/watch?v=WDDMUXa5z4c" },
+      { title: "Loveit?【COVER】", youtubeUrl: "https://www.youtube.com/watch?v=rAHCTLF1f_Y" },
+      { title: "ハートの後味【COVER】", youtubeUrl: "https://www.youtube.com/watch?v=9Pog0yjW4uk" },
+      { title: "ヴァンパイア (The Vampire) - DECO*27 【COVER】", youtubeUrl: "https://www.youtube.com/watch?v=T7lVwCCEmIA" },
+      { title: "TOP(Stray Kids) - TV SIZE【ft. dj-Jo Remix】", youtubeUrl: "https://www.youtube.com/watch?v=ZIPt-jDu47Y" },
+      { title: "Veil -『Fire Force ED』 Keina Suda【COVER】", youtubeUrl: "https://www.youtube.com/watch?v=sfr_jAIyvlY" },
+      { title: "Sarishinohara | サリシノハラ【COVER】", youtubeUrl: "https://www.youtube.com/watch?v=z6DGXvn5B1I" },
+      { title: "蜜月アン・ドゥ・トロワ【COVER】", youtubeUrl: "https://www.youtube.com/watch?v=TrbUdjDsvtI" },
+      { title: "Tear Drop / Milgram 【COVER】", youtubeUrl: "https://www.youtube.com/watch?v=cD2dHbsMAos" },
+      { title: "Nectar【COVER feat. Hanafabuki】", youtubeUrl: "https://www.youtube.com/watch?v=TpH6j8P1vDc" },
+      { title: "恋の魔法-SHORT【COVER】", youtubeUrl: "https://www.youtube.com/watch?v=2irgCXdS82c" },
+      { title: "MIRA - Kanaria 【COVER】", youtubeUrl: "https://www.youtube.com/watch?v=8_JTPACy5Gg" },
+      { title: "花になって - 緑黄色社会【COVER】", youtubeUrl: "https://www.youtube.com/watch?v=twT0WECoRpQ" },
+      { title: "If I Can Stop One Heart From Breaking (Honkai: Star Rail 2.0) ー Cover", youtubeUrl: "https://www.youtube.com/watch?v=J-870Wwx64M" },
     ],
   },
   {
